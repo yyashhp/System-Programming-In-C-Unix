d4bbce2d39a3a4bf0c9615d4eb7e49a0c4f11136
Yash patel <113195427+yyashhp@users.noreply.github.com>
2023-05-17 14:05:44 -0400

    Utilizes File Reading and Writing, String Comparison, Copying, and Concatenation, to detect and write sentences(to a file) that start with "I am"
    The program: 
    Opens a file called MyQualities.txt for writing.
    Repeatedly ...
    Reads a sentence from the keyboard (using fgets).
    Checks if the sentences starts with "I am " (using strstr)
    If it does, then extracts the substring starting at the 5th character, i.e., the substring containing the quality the person believes they possess. (using strncpy).
    Concatenates the substring onto a growing string containing all such qualities, with a comma separator (using strncat - the growing string has a maximal length of 1024 characters).
    Appends the substring to the MyQualities.txt file.
    Stops looping when a string containing just a "." is entered (check using strcmp).
    Closes the file
    Outputs the grown string of qualities to the screen.
    Opens the MyQualities.txt file for reading
    Reads and echo the qualities from the file
    Closes and deletes the file

---
+++ IAm.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdout.h>
+#include <string.h>
+
+#define MAX_STRING_LENGTH 1024;
+typedef char Max_String[Max_String_Length];
+typedef char String[30];
+
+
+void Check_Sentence(String Sentence, Max_String Long_Sentence){
+  if((strstr(Sentence,"I am")!=NULL)){
+    
+    
+   
+
+void Add_Sentence
+
+int main(void) {
+  String Sentence;
+  String Long_Sentence;
+  FILE * SaveFile;
+  String Traits;
+  
+  if ((SaveFile = fopen(Traits,"w+")) == NULL) {
+    perror("Opening people file");
+    exit(EXIT_FAILURE);
+  printf("Please enter sentence, . to end.");
+  scanf("%s",Sentence);
+  while((strcmp(Sentence,"."))){
+    
